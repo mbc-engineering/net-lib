4f63862912f4e237c8b7616a16ca937590356a52
Michael Helfenstein <michael.helfenstein@mbc-engineering.ch>
2018-12-21 13:27:23 +0100

    add foreachasync

---
+++ Source/Mbc.AsyncUtils.Test/IEnumerableExtensionsTest.cs
@@ -0,0 +1,37 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Mbc.AsyncUtils.Test
+{
+    public class IEnumerableExtensionsTest
+    {
+        [Fact]
+        public async Task ForEachAsyncWaits()
+        {
+            // Arrange
+            var results = new List<int>();
+            int i = 0;
+            Task DoSomthing(int counter)
+            {
+                results.Add(counter);
+                return Task.CompletedTask;
+            }
+
+            IEnumerable<Func<Task>> list = new List<Func<Task>> { () => DoSomthing(i++), () => DoSomthing(i++), () => DoSomthing(i++), () => DoSomthing(i++) };
+
+            // Act
+            await list.ForEachAsync(a => a());
+
+            // Assert
+            results.Should().HaveCount(4);
+            results.Should().Equal(Enumerable.Range(0, 4));
+        }
+    }
+}
